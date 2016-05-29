4b2382c867d66f61585d25936f996c2da424d0f0
Simon Jentzsch <simon.jentzsch@slock.it>
2016-05-30 00:11:31 +0200

    use webpack

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './html/index.html';
+import './images/logo.png';
+
+import './css/custom.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'angular-material/angular-material.css';
+
+import './js/voting';
