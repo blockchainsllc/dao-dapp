fef2c7084b8eb29b0ffb8eef1bcd2790308a3abd
Simon Jentzsch <simon.jentzsch@slock.it>
2016-06-02 10:15:37 +0200

    changed progress scale

--- src/js/voting.js
+++ src/js/voting.js
@@ -298,7 +298,8 @@ function DaoVotingCtrl( $scope, $mdDialog, $parse, $filter, $http, $sce) {
       });
    }
    
-   $scope.progress = function(val,total) {
+   $scope.progress = function(val,total, y, n) {
+     total = Math.max(y,n) + (total - y -n)/2;
      return 100 - Math.pow(val/total-1,2)*100;
    }
    
